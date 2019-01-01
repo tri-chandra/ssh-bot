68ae6436c19a3faed192a797309c360019606f78
tri-chandra <tri.chandra@outlook.com>
2019-01-01 18:51:53 +1100

    Volcano added
    .

--- src/api/data/spells/Volcano.js
+++ src/api/data/spells/Volcano.js
@@ -5,14 +5,15 @@ class Volcano extends Spell {
   constructor(level) {
     super({
       name: 'Volcano',
-      type: C.damageSpell,
-      tier: C.Basic,
+      code: 'volcano',
+      type: C.OvertimeSpell,
+      tier: C.Advanced,
       element: C.Fire,
-      damage: 20,
-      speed: C.Normal,
-      count: 1,
-      target: C.Enemy,
-      unlockAt: C.Arena1
+      resistance: 23,
+      overTime: 'You get 12 fire mana at random positions on your playfield. Existing elements are replaced.',
+      unlockAt: [
+        { hero: C.Zenron, level: C.Arena3 }
+      ]
     });
   }
 }
