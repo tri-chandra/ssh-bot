2234f3354e14ba7699327af7fc9df9bff81da4a4
tri-chandra <tri.chandra@outlook.com>
2019-01-06 17:29:11 +1100

    Deck building capability draft 1
    .

--- src/api/routes/v1/deck/index.js
+++ src/api/routes/v1/deck/index.js
@@ -2,7 +2,14 @@ import assembleDeckSnap from '../../../../renderer/functions/assembleDeckSnap';
 
 import Jimp from 'jimp';
 import Zenron from '../../../models/Zenron';
+import Jane from '../../../models/Jane';
+import Thoben from '../../../models/Thoben';
+import Tierra from '../../../models/Tierra';
+import Myris from '../../../models/Myris';
+import VanRaven from '../../../models/VanRaven';
 import Ray from '../../../models/Ray';
+import Lua from '../../../models/Lua';
+import Asgard from '../../../models/Asgard';
 
 async function route(discord, tokens) {
   if (!tokens) return;
@@ -10,10 +17,24 @@ async function route(discord, tokens) {
   const [command, ...rest] = tokens;
 
   let hero;
-  if (['Zenron', 'zenron'].includes(command)) {
+  if (['zenron'].includes(command)) {
     hero = new Zenron();
-  } else if (['Ray', 'ray'].includes(command)) {
+  } else if (['jane'].includes(command)) {
+    hero = new Jane();
+  } else if (['thoben'].includes(command)) {
+    hero = new Thoben();
+  } else if (['tierra'].includes(command)) {
+    hero = new Tierra();
+  } else if (['vanraven', 'van', 'raven'].includes(command)) {
+    hero = new VanRaven();
+  } else if (['asgard'].includes(command)) {
+    hero = new Asgard();
+  } else if (['myris'].includes(command)) {
+    hero = new Myris();
+  } else if (['ray'].includes(command)) {
     hero = new Ray();
+  } else if (['lua'].includes(command)) {
+    hero = new Lua();
   }
 
   if (hero) {
--- src/renderer/functions/assembleDeckSnap.js
+++ src/renderer/functions/assembleDeckSnap.js
@@ -1,5 +1,4 @@
 import Jimp from 'jimp';
-import Zenron from '../../api/models/Zenron';
 
 const width = 1200;
 const height = 500;
@@ -30,33 +29,23 @@ async function appendSpells(img, spells, row) {
   return output;
 }
 
-export default async function run(hero, deckHash, outputFile) {
-  const headerFont = await Jimp.loadFont(Jimp.FONT_SANS_64_BLACK);
+export default async function run(hero, deckHash) {
   const descFont = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-  let bg = await (new Jimp(width, height, 0xFFF9EBFF)).composite(new Jimp(width, headerHeight, 0xFFE9C4FF), 0, 0);
-  const [h, v] = getSpellPosition(1, 1);
-  bg = await bg.composite(new Jimp(((3 * (spellWidth + padding[0])) - padding[0]), 10, 0x968A83FF), h, v - 25);
-  bg = await bg.composite(new Jimp(((3 * (spellWidth + padding[0])) - padding[0]), 10, 0x84B009FF), h + (3 * (spellWidth + padding[0])), v - 25);
-  bg = await bg.composite(new Jimp(((2 * (spellWidth + padding[0])) - padding[0]), 10, 0x3699B9FF), h + (6 * (spellWidth + padding[0])), v - 25);
-  bg = await bg.composite(new Jimp(((1 * (spellWidth + padding[0])) - padding[0]), 10, 0xBA45BBFF), h + (8 * (spellWidth + padding[0])), v - 25);
-
-  const heroImgPath = `./src/renderer/data/images/heroes/${hero.code}/${hero.code}_classic.png`;
-  const heroImg = await (await Jimp.read(heroImgPath)).flip(true, false).scale(0.4);
+  const heroImgPath = `./src/renderer/output/images/heroes/deck-background/${hero.code}.png`;
+  const bg = await Jimp.read(heroImgPath);
 
   const [basic1, basic2, basic3] = hero.spells.basic.map(s => s.sprite);
   const [adv1, adv2, adv3] = hero.spells.advanced.map(s => s.sprite);
   const [elite1, elite2] = hero.spells.elite.map(s => s.sprite);
   const [, , ult1] = hero.spells.ultimate.map(s => s.sprite);
 
-  let result = await bg.composite(heroImg, width - heroImg.bitmap.width, headerHeight / 2);
-  result = await appendSpells(result, [basic1, basic2, basic3, adv1, adv2, adv3, elite1, elite2, ult1], 1);
+  const result = await appendSpells(bg, [basic1, basic2, basic3, adv1, adv2, adv3, elite1, elite2, ult1], 1);
   // result = await appendSpells(result, [adv1, adv2, adv3], 2);
   // result = await appendSpells(result, [elite1, elite2], 3);
   // result = await appendSpells(result, [ult1], 4);
 
   return result
-  .print(headerFont, padding[0], padding[0], 'Zenron')
-  .print(descFont, padding[0], headerHeight + padding[0], 'My description here')
+  .print(descFont, padding[0], headerHeight + padding[0], 'Take a look at my spell deck!')
   .scale(0.3);
   //.write(`${outputFile}/${hero}2.png`);
 };
