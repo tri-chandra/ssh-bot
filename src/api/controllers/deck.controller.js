cad55c472af7bff354694c4d7336ca0eccfb15df
Tri Chandra <tri.chandra@outlook.com>
2019-01-08 13:49:26 +1100

    Deck save + get
    .

--- src/api/controllers/deck.controller.js
+++ src/api/controllers/deck.controller.js
@@ -1,8 +1,59 @@
+import Jimp from 'jimp';
 import { renderDeck } from '../models/deck/ImageProcessor';
 import { get, set } from '../services/redisProvider';
 
+import Zenron from '../models/Zenron';
+import Jane from '../models/Jane';
+import Thoben from '../models/Thoben';
+import Tierra from '../models/Tierra';
+import Myris from '../models/Myris';
+import VanRaven from '../models/VanRaven';
+import Ray from '../models/Ray';
+import Lua from '../models/Lua';
+import Asgard from '../models/Asgard';
+
 export default {
-  async getDeck(hero, deckHash) {
-    return renderDeck(hero, deckHash);
+  async getDeck(discord, command, deckHash) {
+    let hero;
+    if (['zenron'].includes(command)) {
+      hero = new Zenron();
+    } else if (['jane'].includes(command)) {
+      hero = new Jane();
+    } else if (['thoben'].includes(command)) {
+      hero = new Thoben();
+    } else if (['tierra'].includes(command)) {
+      hero = new Tierra();
+    } else if (['vanraven', 'van', 'raven'].includes(command)) {
+      hero = new VanRaven();
+    } else if (['asgard'].includes(command)) {
+      hero = new Asgard();
+    } else if (['myris'].includes(command)) {
+      hero = new Myris();
+    } else if (['ray'].includes(command)) {
+      hero = new Ray();
+    } else if (['lua'].includes(command)) {
+      hero = new Lua();
+    }
+
+    const author = discord.author.id;
+    const target = discord.mentions.members.first() ? discord.mentions.members.first().id : undefined;
+    if (target) {
+      // redis get
+      const hashcode = await get(`${target}:${hero.code}`);
+      const img = await (await renderDeck(hero, hashcode)).getBufferAsync(Jimp.MIME_PNG);
+      discord.channel.send('', {
+        files: [img],
+      });
+    } else if (hero && deckHash.length === 9) {
+      // redis set to author
+      set(`${author}:${hero.code}`, deckHash);
+
+      const img = await (await renderDeck(hero, deckHash)).getBufferAsync(Jimp.MIME_PNG);
+      discord.channel.send('', {
+        files: [img],
+      });
+    } else {
+      discord.reply('the hero is unknown. Type `!help deck` for more information.');
+    }
   },
 };
--- src/api/routes/v1/deck/index.js
+++ src/api/routes/v1/deck/index.js
@@ -1,56 +1,11 @@
-import Jimp from 'jimp';
-
 import DeckController from '../../../controllers/deck.controller';
-
-import Zenron from '../../../models/Zenron';
-import Jane from '../../../models/Jane';
-import Thoben from '../../../models/Thoben';
-import Tierra from '../../../models/Tierra';
-import Myris from '../../../models/Myris';
-import VanRaven from '../../../models/VanRaven';
-import Ray from '../../../models/Ray';
-import Lua from '../../../models/Lua';
-import Asgard from '../../../models/Asgard';
 
 async function route(discord, tokens) {
   if (!tokens) return;
 
   const [command, hashcode, ...rest] = tokens;
 
-  let hero;
-  if (['zenron'].includes(command)) {
-    hero = new Zenron();
-  } else if (['jane'].includes(command)) {
-    hero = new Jane();
-  } else if (['thoben'].includes(command)) {
-    hero = new Thoben();
-  } else if (['tierra'].includes(command)) {
-    hero = new Tierra();
-  } else if (['vanraven', 'van', 'raven'].includes(command)) {
-    hero = new VanRaven();
-  } else if (['asgard'].includes(command)) {
-    hero = new Asgard();
-  } else if (['myris'].includes(command)) {
-    hero = new Myris();
-  } else if (['ray'].includes(command)) {
-    hero = new Ray();
-  } else if (['lua'].includes(command)) {
-    hero = new Lua();
-  }
-
-  const author = discord.author.id;
-  const target = discord.mentions.members.first() ? discord.mentions.members.first().id : undefined;
-  if (target) {
-    // redis get
-  } else if (hero && hashcode.length === 9) {
-    // redis set to author
-    const img = await (await DeckController.getDeck(hero, hashcode)).getBufferAsync(Jimp.MIME_PNG);
-    discord.channel.send('', {
-      files: [img],
-    });
-  } else {
-    discord.reply('the hero is unknown. Type `!help deck` for more information.');
-  }
+  DeckController.getDeck(discord, command, hashcode)
 }
 
 export default {
